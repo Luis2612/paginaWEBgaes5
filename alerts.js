48f20a8018e57e71c00e44b1230a7554aeeb6ab1
Luis2612 <99561037+Luis2612@users.noreply.github.com>
2022-07-14 18:25:41 -0500

    Se agrega el proyecto

---
+++ alerts.js
@@ -0,0 +1,91 @@
+function confirmar1 ()
+{
+            Swal.fire({
+              title: '¿Seguro que desea ingresar:?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Confirmar'
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                
+                window.location='proveedores.html';
+                
+              } 
+            })
+}
+
+function confirmar2()
+{
+            Swal.fire({
+              title: '¿Seguro que desea ingresar:?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Confirmar'
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                
+                window.location='inventario.html';
+                
+              } 
+            })
+}
+function confirmar3()
+{
+            Swal.fire({
+              title: '¿Seguro que desea ingresar:?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Confirmar'
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                
+                window.location='fidelizacion.html';
+                
+              } 
+            })
+}
+function confirmar4()
+{
+            Swal.fire({
+              title: '¿Seguro que desea ingresar:?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Confirmar'
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                
+                window.location='controldeobra.html';
+                
+              } 
+            })
+}
+function confirmar5()
+{
+            Swal.fire({
+              title: '¿Seguro que desea ingresar:?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Confirmar'
+            }).then((result) => {
+              /* Read more about isConfirmed, isDenied below */
+              if (result.isConfirmed) {
+                
+                window.location='novedades.html';
+                
+              } 
+            })
+}
